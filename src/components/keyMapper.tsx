56e6b94207e7e8fb7967603c40e5eb52a29bf103
juliteGarden <111869677+gerAoMidori@users.noreply.github.com>
2026-01-29 16:14:06 +0100

    Key mapping logic setup - start

--- src/components/keyMapper.tsx
+++ src/components/keyMapper.tsx
@@ -1,4 +1,4 @@
-import { IconBrandWindows } from "@tabler/icons-react"
+
 import { Input } from "./ui/input";
 
 import Key from "./keyboard-components/key"
@@ -27,76 +27,79 @@ const AllKeys = [
   { id: "f12", label: "F12", value: "F12", width: 1, type: "Basic" },
 
 
-  { id:"grave", label:"²", value:"Backquote", width:1, type:"Basic" },
-  { id:"1", label:"&", value:"Digit1", width:1, type:"Basic" },
-  { id:"2", label:"é", value:"Digit2", width:1, type:"Basic" },
-  { id:"3", label:"\"", value:"Digit3", width:1, type:"Basic" },
-  { id:"4", label:"'", value:"Digit4", width:1, type:"Basic" },
-  { id:"5", label:"(", value:"Digit5", width:1, type:"Basic" },
-  { id:"6", label:"-", value:"Digit6", width:1, type:"Basic" },
-  { id:"7", label:"è", value:"Digit7", width:1, type:"Basic" },
-  { id:"8", label:"_", value:"Digit8", width:1, type:"Basic" },
-  { id:"9", label:"ç", value:"Digit9", width:1, type:"Basic" },
-  { id:"0", label:"à", value:"Digit0", width:1, type:"Basic" },
-  { id:"minus", label:")", value:"Minus", width:1, type:"Basic" },
-  { id:"equal", label:"=", value:"Equal", width:1, type:"Basic" },
+  { id: "grave", label: "²", value: "Backquote", width: 1, type: "Basic" },
+  { id: "1", label: "&", value: "Digit1", width: 1, type: "Basic" },
+  { id: "2", label: "é", value: "Digit2", width: 1, type: "Basic" },
+  { id: "3", label: "\"", value: "Digit3", width: 1, type: "Basic" },
+  { id: "4", label: "'", value: "Digit4", width: 1, type: "Basic" },
+  { id: "5", label: "(", value: "Digit5", width: 1, type: "Basic" },
+  { id: "6", label: "-", value: "Digit6", width: 1, type: "Basic" },
+  { id: "7", label: "è", value: "Digit7", width: 1, type: "Basic" },
+  { id: "8", label: "_", value: "Digit8", width: 1, type: "Basic" },
+  { id: "9", label: "ç", value: "Digit9", width: 1, type: "Basic" },
+  { id: "0", label: "à", value: "Digit0", width: 1, type: "Basic" },
+  { id: "minus", label: ")", value: "Minus", width: 1, type: "Basic" },
+  { id: "equal", label: "=", value: "Equal", width: 1, type: "Basic" },
 
 
-  { id:"tab", label:"Tab", value:"Tab", width:1, type:"Basic" },
-  { id:"a", label:"A", value:"KeyA", width:1, type:"Basic" },
-  { id:"z", label:"Z", value:"KeyZ", width:1, type:"Basic" },
-  { id:"e", label:"E", value:"KeyE", width:1, type:"Basic" },
-  { id:"r", label:"R", value:"KeyR", width:1, type:"Basic" },
-  { id:"t", label:"T", value:"KeyT", width:1, type:"Basic" },
-  { id:"y", label:"Y", value:"KeyY", width:1, type:"Basic" },
-  { id:"u", label:"U", value:"KeyU", width:1, type:"Basic" },
-  { id:"i", label:"I", value:"KeyI", width:1, type:"Basic" },
-  { id:"o", label:"O", value:"KeyO", width:1, type:"Basic" },
-  { id:"p", label:"P", value:"KeyP", width:1, type:"Basic" },
-  { id:"lbracket", label:"^", value:"BracketLeft", width:1, type:"Basic" },
-  { id:"rbracket", label:"$", value:"BracketRight", width:1, type:"Basic" },
-  { id:"capslock", label:"Caps", value:"CapsLock", width:1, type:"Basic" },
-  { id:"q", label:"Q", value:"KeyQ", width:1, type:"Basic" },
-  { id:"s", label:"S", value:"KeyS", width:1, type:"Basic" },
-  { id:"d", label:"D", value:"KeyD", width:1, type:"Basic" },
-  { id:"f", label:"F", value:"KeyF", width:1, type:"Basic" },
-  { id:"g", label:"G", value:"KeyG", width:1, type:"Basic" },
-  { id:"h", label:"H", value:"KeyH", width:1, type:"Basic" },
-  { id:"j", label:"J", value:"KeyJ", width:1, type:"Basic" },
-  { id:"k", label:"K", value:"KeyK", width:1, type:"Basic" },
-  { id:"l", label:"L", value:"KeyL", width:1, type:"Basic" },
-  { id:"m", label:"M", value:"KeyM", width:1, type:"Basic" },
-  { id:"ù", label:"Ù", value:"Quote", width:1, type:"Basic" },
+  { id: "tab", label: "Tab", value: "Tab", width: 1, type: "Basic" },
+  { id: "a", label: "A", value: "KeyA", width: 1, type: "Basic" },
+  { id: "z", label: "Z", value: "KeyZ", width: 1, type: "Basic" },
+  { id: "e", label: "E", value: "KeyE", width: 1, type: "Basic" },
+  { id: "r", label: "R", value: "KeyR", width: 1, type: "Basic" },
+  { id: "t", label: "T", value: "KeyT", width: 1, type: "Basic" },
+  { id: "y", label: "Y", value: "KeyY", width: 1, type: "Basic" },
+  { id: "u", label: "U", value: "KeyU", width: 1, type: "Basic" },
+  { id: "i", label: "I", value: "KeyI", width: 1, type: "Basic" },
+  { id: "o", label: "O", value: "KeyO", width: 1, type: "Basic" },
+  { id: "p", label: "P", value: "KeyP", width: 1, type: "Basic" },
+  { id: "lbracket", label: "^", value: "BracketLeft", width: 1, type: "Basic" },
+  { id: "rbracket", label: "$", value: "BracketRight", width: 1, type: "Basic" },
+  { id: "capslock", label: "Caps", value: "CapsLock", width: 1, type: "Basic" },
+  { id: "q", label: "Q", value: "KeyQ", width: 1, type: "Basic" },
+  { id: "s", label: "S", value: "KeyS", width: 1, type: "Basic" },
+  { id: "d", label: "D", value: "KeyD", width: 1, type: "Basic" },
+  { id: "f", label: "F", value: "KeyF", width: 1, type: "Basic" },
+  { id: "g", label: "G", value: "KeyG", width: 1, type: "Basic" },
+  { id: "h", label: "H", value: "KeyH", width: 1, type: "Basic" },
+  { id: "j", label: "J", value: "KeyJ", width: 1, type: "Basic" },
+  { id: "k", label: "K", value: "KeyK", width: 1, type: "Basic" },
+  { id: "l", label: "L", value: "KeyL", width: 1, type: "Basic" },
+  { id: "m", label: "M", value: "KeyM", width: 1, type: "Basic" },
+  { id: "ù", label: "Ù", value: "Quote", width: 1, type: "Basic" },
+
+  { id: "shift", label: "Shift", value: "ShiftLeft", width: 1, type: "Extended" },
+  { id: "shift_r", label: "Shift", value: "ShiftRight", width: 1, type: "Extended" },
+  { id: "ctrl", label: "Ctrl", value: "ControlLeft", width: 1, type: "Extended" },
+  { id: "alt", label: "Alt", value: "AltLeft", width: 1, type: "Extended" },
+  { id: "altgr", label: "AltGr", value: "AltRight", width: 1, type: "Extended" },
+  { id: "win", label: "Win", value: "win", width: 1, type: "Extended" },
+  { id: "fn", label: "Fn", value: "Fn", width: 1, type: "Extended" },
 
 
-  { id:"shift", label:"Shift", value:"ShiftLeft", width:1, type:"Extended" },
-  { id:"shift_r", label:"Shift", value:"ShiftRight", width:1, type:"Extended" },
-  { id:"ctrl", label:"Ctrl", value:"ControlLeft", width:1, type:"Extended" },
-  { id:"alt", label:"Alt", value:"AltLeft", width:1, type:"Extended" },
-  { id:"altgr", label:"AltGr", value:"AltRight", width:1, type:"Extended" },
-  { id:"win", label: <IconBrandWindows />, value:"win", width:1, type:"Extended" },
-  { id:"fn", label:"Fn", value:"Fn", width:1, type:"Extended" },
+  { id: "space", label: "Space", value: "Space", width: 1, type: "Basic" },
+  { id: "enter", label: "Enter", value: "Enter", width: 1, type: "Extended" },
+  { id: "backspace", label: "Backspace", value: "Backspace", width: 1, type: "Extended" },
 
 
-  { id:"space", label:" ", value:"Space", width:1, type:"Basic" },
-  { id:"enter", label:"Enter", value:"Enter", width:1, type:"Extended" },
-  { id:"backspace", label:"Backspace", value:"Backspace", width:1, type:"Extended" },
+  { id: "pgup", label: "PgUp", value: "PageUp", width: 1, type: "Extended" },
+  { id: "pgdn", label: "PgDn", value: "PageDown", width: 1, type: "Extended" },
+  { id: "home", label: "Home", value: "Home", width: 1, type: "Extended" },
+  { id: "up", label: "↑", value: "ArrowUp", width: 1, type: "Extended" },
+  { id: "left", label: "←", value: "ArrowLeft", width: 1, type: "Extended" },
+  { id: "down", label: "↓", value: "ArrowDown", width: 1, type: "Extended" },
+  { id: "right", label: "→", value: "ArrowRight", width: 1, type: "Extended" },
 
 
-  { id:"pgup", label:"PgUp", value:"PageUp", width:1, type:"Extended" },
-  { id:"pgdn", label:"PgDn", value:"PageDown", width:1, type:"Extended" },
-  { id:"home", label:"Home", value:"Home", width:1, type:"Extended" },
-  { id:"up", label:"↑", value:"ArrowUp", width:1, type:"Extended" },
-  { id:"left", label:"←", value:"ArrowLeft", width:1, type:"Extended" },
-  { id:"down", label:"↓", value:"ArrowDown", width:1, type:"Extended" },
-  { id:"right", label:"→", value:"ArrowRight", width:1, type:"Extended" },
-
-
-  { id:"comma", label:",", value:"Comma", width:1, type:"Basic" },
-  { id:"semicolon", label:";", value:"Semicolon", width:1, type:"Basic" },
-  { id:"colon", label:":", value:"Period", width:1, type:"Basic" },
-  { id:"excl", label:"!", value:"Slash", width:1, type:"Basic" },
+  { id: "comma", label: ",", value: "Comma", width: 1, type: "Basic" },
+  { id: "semicolon", label: ";", value: "Semicolon", width: 1, type: "Basic" },
+  { id: "colon", label: ":", value: "Period", width: 1, type: "Basic" },
+  { id: "excl", label: "!", value: "Slash", width: 1, type: "Basic" },
 ];
+
+function run() {
+  console.log("run");
+}
 
 
 export default function KeyMapper() {
@@ -116,25 +119,30 @@ export default function KeyMapper() {
 
       {Object.entries(groupedKeys).map(([type, keys]) => (
         <Accordion defaultValue={["Basic"]} className="w-full mt-4">
-        <AccordionItem value={type}>
-          <AccordionTrigger>{type}</AccordionTrigger>
-          <AccordionContent>
-             {keys.map((keyData) => ( 
-              <Key
-                key={keyData.id}
-                id={keyData.id}
-                label={keyData.label}
-                width={keyData.width}
-                value={keyData.value}
-              />
-            ))}
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+          <AccordionItem value={type}>
+            <AccordionTrigger>{type}</AccordionTrigger>
+            <AccordionContent>
+              {keys.map((keyData) => (
+                <div className="inline-block m-0.5" key={keyData.id}>
+                  <Key
+                    key={keyData.id}
+                    id={keyData.id}
+                    label={keyData.label}
+                    width={keyData.width}
+                    value={keyData.value}
+
+                  />
+
+                </div>
+
+              ))}
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       ))}
 
 
-      
+
     </main>
   )
 }
--- src/components/keyboard-components/key.tsx
+++ src/components/keyboard-components/key.tsx
@@ -5,9 +5,10 @@ interface KeyProps {
   label: string | React.ReactNode
   value: string 
   width: number
+  onSelect?: (id: string) => void;
 }
 
-function Key({ id, label, value, width }: KeyProps) {
+function Key({ id, label, value, width, onSelect }: KeyProps) {
   const widthPixels = (12 * width + (width - 1)) * 4
 
   return (
@@ -17,6 +18,8 @@ function Key({ id, label, value, width }: KeyProps) {
       variant="outline"
       style={{ width: `${widthPixels}px` }}
       className="h-12 bg-transparent inset-shadow-2xs active:bg-blue-300 text-black rounded px-2 py-1 text-xs font-semibold transition-colors border border-gray-200"
+      onClick={() => onSelect?.(id)}
+
     >
       <span>{label}</span>
     </Button>
