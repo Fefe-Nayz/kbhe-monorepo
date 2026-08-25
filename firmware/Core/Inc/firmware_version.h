cecb0d7079e08c4999e32c60a373e62134018e1d
Fefedu973 <80718477+Fefedu973@users.noreply.github.com>
2026-08-25 20:38:06 +0200

    release(firmware): v2.0.10

--- firmware/Core/Inc/firmware_version.h
+++ firmware/Core/Inc/firmware_version.h
@@ -5,7 +5,7 @@
 
 #define FIRMWARE_VERSION_MAJOR 2u
 #define FIRMWARE_VERSION_MINOR 0u
-#define FIRMWARE_VERSION_PATCH 9u
+#define FIRMWARE_VERSION_PATCH 10u
 #define FIRMWARE_VERSION_PACKED                                                \
   (((uint32_t)FIRMWARE_VERSION_MAJOR << 16) |                                  \
    ((uint32_t)FIRMWARE_VERSION_MINOR << 8) |                                   \
